26df1f6fe22da193f15b17eb234ebe49482e2d1a
jixiangup <jixiangup@gmail.com>
2025-04-15 18:34:13 +0800

    Use cli::run() fn to start the application

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use crawler::CrawlerCommand;
+use trpl_core::command::Command;
 
 #[derive(Parser)]
 #[command(name = "trpl", version="1.0.0", author="Jixon", about="一个支持多功能的 CLI 工具. 编写了一些自己的 Rust 代码来实现一些功能来加深 Rust 印象.")]
@@ -11,4 +12,17 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum Commands {
     Crawler(CrawlerCommand),
+}
+
+pub async fn run() {
+    let cli = Cli::parse();
+    command_factory(cli).await;
+}
+
+async fn command_factory(cli: Cli) {
+    match cli.command {
+        Commands::Crawler(cmd) => {
+            cmd.run().await;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-use clap::Parser;
-use cli::{Cli, Commands};
-use trpl_core::command::Command;
-
 #[tokio::main]
 async fn main() {
-    let cli = Cli::parse();
-    match cli.command {
-        Commands::Crawler(cmd) => {
-            cmd.run().await;
-        }
-    }
+    cli::run().await;
 }
-
